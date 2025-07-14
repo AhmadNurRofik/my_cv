0ace5528f0b48019c6f8983a2c34bebcd5472a77
AhmadNurRofik <ahmadnurrofik@students.amikom.ac.id>
2025-07-14 22:05:13 +0700

    ganti

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const router = createRouter({
   history: createWebHistory('/'),
